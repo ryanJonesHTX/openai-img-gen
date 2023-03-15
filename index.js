59ac1eaa0d31cc566de6136e36b13a4a9c1826fd
Ryan Jones <RyanJonesHTX@pm.me>
2023-03-14 22:56:38 -0500

    OpenAI Image Gen - Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 3000
+
+const app = express()
+
+//enable body parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.listen(port, () => console.log(`Server started on Port ${port}`))
+
+app.use('/openai', require('./routes/openaiRoutes'))
